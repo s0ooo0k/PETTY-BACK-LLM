1250e8f54e25008ecd042187d8d437c0ae9d8b95
Juyoung8563 <zxcvbnm856311@gmail.com>
2025-04-21 23:53:38 +0900

    [refactor] Gemini API 호출 로직 삭제, promptGeneratorService 호출 로직 작성, Together API 프롬프트 수정

--- src/main/java/io/github/petty/pipeline/controller/PipelineController.java
+++ src/main/java/io/github/petty/pipeline/controller/PipelineController.java
@@ -1,6 +1,6 @@
 package io.github.petty.pipeline.controller;
 
-import io.github.petty.pipeline.service.GeminiService;
+import io.github.petty.pipeline.service.PromptGeneratorService;
 import io.github.petty.pipeline.service.TogetherService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PipelineController {
 
     private final TogetherService togetherService;
-    private final GeminiService geminiService;
+    private final PromptGeneratorService promptGeneratorService;
 
     @GetMapping("/pipeline")
     public String showPipelineForm() {
@@ -30,35 +30,20 @@ public class PipelineController {
             Model model
     ) {
         try {
-            // 1. Together API로 반려동물 정보 추출
             String extractedPetInfoJson = togetherService.answer(
-                    visionReport + " -> 이 문장에서 반려동물의 이름, 종, 무게, 맹수 여부(only true or false)를 JSON 형식으로 작성 + " +
-                            "만약 반려동물의 종과 무게를 보았을 때, 입마개가 필요할 것 같다면 맹수 여부를 'true'로 작성"
+                    visionReport + " -> 이 문장에서 반려동물의 이름(name), 종(species), 무게(weight), 맹수 여부(is_danger(only true or false))를 JSON 형식으로 작성 + " +
+                            "만약 반려동물의 종과 무게를 보았을 때, 입마개가 필요할 것 같다면 맹수 여부를 'true'로 작성 + " + "무게는 kg 단위를 반드시 포함"
             );
 
-            // 2. Gemini API 호출 프롬프트 작성
-            String prompt = String.format("""
-                    반려동물 JSON 정보:
-                    %s
-                    
-                    대한민국 %s 지역의 반려동물 동반 여행지를 추천받기 위한 프롬프트를 작성.
-                    반려동물 정보는 텍스트로 작성.
-                    상단에 정확한 정식 행정 구역명(시, 군, 구)과 반려동물 정보를 작성.
-                    정확한 정식 행정 구역명(시, 군, 구)을 기반으로 작성(예: 인천광역시 부평구).
-                    평문으로 작성 no markdown.
-                    """, extractedPetInfoJson, location);
+            String prompt = promptGeneratorService.generatePrompt(extractedPetInfoJson, location);
 
-            // 3. Gemini API로 추천 요청
-            String recommendation = geminiService.forRAG(prompt);
+            model.addAttribute("recommendation", prompt); // 실제 추천 대신 prompt만 전달
 
-            // 4. View로 결과 전달
-            model.addAttribute("extractedPetInfo", extractedPetInfoJson);
-            model.addAttribute("recommendation", recommendation);
             return "pipeline";
 
         } catch (Exception e) {
-            log.error("추천 처리 중 오류 발생", e);
-            model.addAttribute("error", "추천 결과를 받아오지 못했습니다.");
+            log.error("프롬프트 제작 중 오류 발생", e);
+            model.addAttribute("error", "결과를 받아오지 못했습니다.");
             return "pipeline";
         }
     }
