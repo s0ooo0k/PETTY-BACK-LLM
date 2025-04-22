2948e144d112e896612d02532d1fe9eaeb52e509
Juyoung8563 <zxcvbnm856311@gmail.com>
2025-04-22 17:32:29 +0900

    [refactor] PipelineController Together API 프롬프트 수정

--- src/main/java/io/github/petty/pipeline/controller/PipelineController.java
+++ src/main/java/io/github/petty/pipeline/controller/PipelineController.java
@@ -32,7 +32,7 @@ public class PipelineController {
         try {
             String extractedPetInfoJson = togetherService.answer(
                     visionReport + " -> 이 문장에서 반려동물의 이름(name), 종(species), 무게(weight), 맹수 여부(is_danger(only true or false))를 JSON 형식으로 작성 + " +
-                            "만약 반려동물의 종과 무게를 보았을 때, 입마개가 필요할 것 같다면 맹수 여부를 'true'로 작성 + " + "무게는 kg 단위를 반드시 포함"
+                            "만약 반려동물의 종과 무게를 보았을 때, 입마개가 필요할 것 같다면 맹수 여부를 'true'로 작성 + " + "무게는 kg 단위를 반드시 포함" + "no markdown, only JSON"
             );
 
             String prompt = promptGeneratorService.generatePrompt(extractedPetInfoJson, location);
