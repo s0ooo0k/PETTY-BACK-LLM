559487430983ddfba2f371930afa26778aeef7f5
Juyoung8563 <zxcvbnm856311@gmail.com>
2025-04-22 20:52:44 +0900

    [refactor] Controller에서 관리하던 프롬프트를 포함한 로직을 PromptAssemblyService로 책임 분리하여 구현 및 리팩터링

--- src/main/java/io/github/petty/pipeline/controller/PipelineController.java
+++ src/main/java/io/github/petty/pipeline/controller/PipelineController.java
@@ -1,5 +1,6 @@
 package io.github.petty.pipeline.controller;
 
+import io.github.petty.pipeline.service.PromptAssemblyService;
 import io.github.petty.pipeline.service.PromptGeneratorService;
 import io.github.petty.pipeline.service.TogetherService;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class PipelineController {
 
-    private final TogetherService togetherService;
-    private final PromptGeneratorService promptGeneratorService;
+    private final PromptAssemblyService promptAssemblyService;
 
     @GetMapping("/pipeline")
     public String showPipelineForm() {
@@ -25,20 +25,15 @@ public class PipelineController {
 
     @PostMapping("/pipeline")
     public String processPipeline(
+            // form 으로 받은 임시 Vision 보고서
             @RequestParam("visionReport") String visionReport,
+            // form 으로 받은 사용자 위치
             @RequestParam("location") String location,
             Model model
     ) {
         try {
-            String extractedPetInfoJson = togetherService.answer(
-                    visionReport + " -> 이 문장에서 반려동물의 이름(name), 종(species), 무게(weight), 맹수 여부(is_danger(only true or false))를 JSON 형식으로 작성 + " +
-                            "만약 반려동물의 종과 무게를 보았을 때, 입마개가 필요할 것 같다면 맹수 여부를 'true'로 작성 + " + "무게는 kg 단위를 반드시 포함" + "no markdown, only JSON" + "최종 결과에 {}는 제거"
-            );
-
-            String prompt = promptGeneratorService.generatePrompt(extractedPetInfoJson, location);
-
-            model.addAttribute("recommendation", prompt); // 실제 추천 대신 prompt만 전달
-
+            String prompt = promptAssemblyService.assemblePrompt(visionReport, location);
+            model.addAttribute("recommendation", prompt);
             return "pipeline";
 
         } catch (Exception e) {
